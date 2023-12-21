9abb2d492a85ec021459e9f098d767f8806980b7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-21 09:03:14 +0100

    msm8953-common: rootdir: Initial libperfmgr init config
    * From coral, heavily stripped down
    
    Change-Id: Ib6846e4f4f23baf56910d2330e26846b99830218
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- msm8953.mk
+++ msm8953.mk
@@ -210,6 +210,7 @@ PRODUCT_PACKAGES += \
     init.mmi.usb.rc \
     init.mmi.rc \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
