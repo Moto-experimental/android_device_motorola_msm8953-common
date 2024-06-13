6d06b8881dcf5f0e544f68c7c1dce5cce92c0c13
SirRGB <sirrgb@proton.me>
2024-06-13 20:55:42 +0200

    msm8953-common: move recovery init scripts to root
    * init: Unable to read config file '/init.recovery.qcom.rc': open() failed: No such file or directory
    * Im too stupid for blueprints
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,3 +9,11 @@ LOCAL_SRC_FILES    := root/ueventd.qcom.rc
 # this needs to be in /vendor/etc/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
