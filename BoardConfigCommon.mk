8a83d3958a95baf157a416474e522f4f6c05b981
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    msm8953-common: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -120,6 +120,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN := \
