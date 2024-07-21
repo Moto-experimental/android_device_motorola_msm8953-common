aniketghodake10/android_device_xiaomi_msm8953-common

msm8953-common: Adding Audio HAL V5: Direction API
Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
Signed-off-by: PrateekPunetha <prateekpunetha@gmail.com>

msm8953.mk
@@ -78,9 +78,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \

Moto-experimental/android_device_motorola_msm8953-common

msm8953.mk
@@ -38,9 +38,9 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl_32 \
+    android.hardware.audio@5.0-impl_32 \
     android.hardware.audio.service_32 \
-    android.hardware.audio.effect@4.0-impl_32 \
+    android.hardware.audio.effect@5.0-impl_32 \
     android.hardware.soundtrigger@2.1-impl_32 \
     audio.r_submix.default \
     audio.usb.default \