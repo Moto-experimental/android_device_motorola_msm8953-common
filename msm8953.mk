0d47c61ac2ac605ae0dc28dcc5003dba852eee79
SirRGB <sirrgb@proton.me>
2026-07-26 13:13:44 +0200

    msm8953-common: decommonize shipping api

--- msm8953.mk
+++ msm8953.mk
@@ -16,7 +16,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit proprietary files
 $(call inherit-product, vendor/motorola/msm8953-common/msm8953-common-vendor.mk)
